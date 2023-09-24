0c63ad749bb0e193058b165ad858236c3610a591
Arijit2811 <arijitrollno28@gmail.com>
2023-09-24 16:19:22 +0530

    1st commit

---
+++ src/main/java/com/test/jenkinsproject/JenkinsProjectApplication.java
@@ -0,0 +1,20 @@
+package com.test.jenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JenkinsProjectApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder.sources(JenkinsProjectApplication.class));
+	}
+
+}
---
+++ src/main/java/com/test/jenkinsproject/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.test.jenkinsproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/test")
+	public String getVal() {
+		return "Hi Arijit. Application is running ";
+	}
+	
+}
